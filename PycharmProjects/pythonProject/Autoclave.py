ae889c8276136f6d6d4a96523f6b5be113f12b09
esteban11519 <eladinof@unal.edu.co>
2021-03-28 07:02:09 -0500

    Comentarios

--- PycharmProjects/pythonProject/Autoclave.py
+++ PycharmProjects/pythonProject/Autoclave.py
@@ -1,41 +1,53 @@
-class Autoclave:
+class Autoclave: # Se crea la clase autoclave
     def __init__(self, temp, open, close, presion, humedad):
+        
+        # Se definen 5 características del objeto
+        
         self.temperature = temp
         self.open = open
         self.close = close
         self.pressure = presion
         self.humidity = humedad
 
+    # Se define el método de abrir 
     def abrir(self):
         if self.open == 0:
             self.open = 1
             self.close = 0
         print("self.open")
-
+    
+    # Se define el método de cerrar
     def cerrar(self):
         if self.close == 0:
             self.open = 0
             self.close = 1
         print("self.close")
 
+    # Se configura un método para ajustar la temperatura
     def setTemp(self,temp):
         self.temperature = temp
-
+        
+    # Se ajusta la presión
     def setPressure(self,pres):
         self.pressure = pres
-
+        
+    # Se ajusta la humedad
     def sethumidity(self,hum):
         self.humidity = hum
-        
+    
+    # Permite obtener la temperatura
     def getTemp(self):
         return self.temperature
-
+    
+    # Se obtiene la presión
     def getPressure(self):
         return self.pressure
 
+    # Se obtiene la humedad
     def getHumidity(self):
         return self.humidity
     
+    # Permite saber si está abierto o no
     def getEstado(self):
         if self.open==1:
             return "Abierto"
--- PycharmProjects/pythonProject/main.py
+++ PycharmProjects/pythonProject/main.py
@@ -11,26 +11,26 @@ class window(QMainWindow):
         
         
         # Variables de ajuste
-        t_printData=1000  #Tiempo de recolección de datos en milisegundos
-        self.Temp_maxima=100
-        self.Pressure_maxima=5
-        self.guardarDatos="memoria.txt"
+        t_printData=1000  #Periodo de recolección de datos en milisegundos
+        self.Temp_maxima=100 # Temperatura máxima en grados centígrados
+        self.Pressure_maxima=5 # Presión máxima en atm
+        self.guardarDatos="memoria.txt" # Nombre del archivo en el que se va a guardar el registro
         # Inicio del código
         
-        
+        # Se configuran las dimensiones de la pantalla principal
         super(window,self).__init__()
         self.setGeometry(200,200,700,700)
         self.setWindowTitle("Autoclave")
-        #Objeto Autoclave
+        #Objeto Autoclave es instanceado
         self.AutoclaveBog = Autoclave.Autoclave(16, 0, 1, 0.74, 0.46)
 
-        # CreacionBoton1
+        # CreacionBoton1 para abrir
         self.b1 = QtWidgets.QPushButton(self)
         self.b1.setText("Abrir")
         self.b1.clicked.connect(self.abrirEnc)
         self.b1.move(40,85)
 
-        #Creacion ImagenAutoClave
+        #Creacion Imagen AutoClave
         self.l2 = QtWidgets.QLabel(self)
         self.pixmap1 = QtGui.QPixmap("AutoClaveCerrado.png")
         self.pixmap2 = QtGui.QPixmap("AutoClaveAbierto.png")
@@ -38,31 +38,31 @@ class window(QMainWindow):
         self.l2.setPixmap(self.pixmap1)
         self.l2.setAlignment(Qt.AlignCenter)
 
-        # CreacionBoton2
+        # CreacionBoton2 para cerrar el autoclave
         self.b2 = QtWidgets.QPushButton(self)
         self.b2.setText("Cerrar")
         self.b2.clicked.connect(self.cerrarEnc)
         self.b2.move(40, 125)
 
-        # CreacionLabel
+        # CreacionLabel para la puerta
         self.l1 = QtWidgets.QLabel(self)
         self.l1.setText("Puerta:")
         self.l1.setAlignment(Qt.AlignLeft)
         self.l1.setFont(QtGui.QFont('SansSerif',12))
         self.l1.move(20,50)
 
-        # CreacionLabel
+        # CreacionLabel para la temperatura
         self.l3 = QtWidgets.QLabel(self)
         self.l3.setText("Temperatura:")
         self.l3.setAlignment(Qt.AlignLeft)
         self.l3.setFont(QtGui.QFont('SansSerif', 12))
         self.l3.setGeometry(20,180,110,100)
 
-            #CreacionTextBoxTemp
+            #CreacionTextBoxTemp que permite captar la información deseada.
         self.tb1 = QtWidgets.QLineEdit(self)
         self.tb1.setGeometry(90,208,50,20)
 
-            # CreacionBoton2
+            # CreacionBoton2 Permite capturar la información de la temperatura. 
         self.b3 = QtWidgets.QPushButton(self)
         self.b3.setText("Ok.")
         self.b3.clicked.connect(self.cambioTemp)
@@ -70,7 +70,7 @@ class window(QMainWindow):
         
         
         
-        # CreacionBoton4 Blanquear memoria
+        # CreacionBoton4 para reiniciar el registro de información
         self.b4 = QtWidgets.QPushButton(self)
         self.b4.setText("Reiniciar registro")
         self.b4.clicked.connect(self.reiniciarRegistro)
@@ -78,20 +78,20 @@ class window(QMainWindow):
         
         
 
-        # CreacionLabel
+        # CreacionLabel para configurar la tempertatura
         self.l3 = QtWidgets.QLabel(self)
         self.l3.setText("SetTemp:")
         self.l3.setAlignment(Qt.AlignLeft)
         self.l3.setFont(QtGui.QFont('SansSerif', 8))
         self.l3.setGeometry(35,210,50,20)
 
-        #Progress Bar
+        #Progress Bar es un indicativo de la temperatura.
         self.pb = QtWidgets.QProgressBar(self)
         self.pb.setGeometry(65,280,50,200)
         self.pb.setValue((self.AutoclaveBog.temperature/150)*100)
         self.pb.setOrientation(Qt.Vertical)
 
-        # CreacionLabel
+        # CreacionLabel Indica la tempertarura máxima que puede alcanzar.
         self.l4 = QtWidgets.QLabel(self)
         self.l4.setText("150 " +str(chr(176)) + "C")
         self.l4.setAlignment(Qt.AlignLeft)
@@ -105,14 +105,14 @@ class window(QMainWindow):
         self.BCD.display(self.AutoclaveBog.temperature)
         self.BCD.setDigitCount(5)
 
-        # CreacionLabel
+        # CreacionLabel para grados centígrados
         self.l5 = QtWidgets.QLabel(self)
         self.l5.setText(str(chr(176)) + "C")
         self.l5.setAlignment(Qt.AlignLeft)
         self.l5.setFont(QtGui.QFont('SansSerif', 16))
         self.l5.setGeometry(100, 508, 100, 100)
 
-        # CreacionLabel
+        # CreacionLabel, se muestra presión en la ventana
         self.l6 = QtWidgets.QLabel(self)
         self.l6.setText("Presi"+str(chr(243))+"n: ")
         self.l6.setAlignment(Qt.AlignLeft)
@@ -125,31 +125,32 @@ class window(QMainWindow):
         self.BCD1.display(self.AutoclaveBog.pressure)
         self.BCD1.setDigitCount(5)
         
-        # Referente a la presion
-        # CreacionLabel
+    
+        # CreacionLabel para unidades de la presióñ
         self.l7 = QtWidgets.QLabel(self)
         self.l7.setText("atm")
         self.l7.setAlignment(Qt.AlignLeft)
         self.l7.setFont(QtGui.QFont('SansSerif', 16))
         self.l7.setGeometry(105, 610, 100, 100)
 
-        # CreacionLabel
+        # CreacionLabel para indicar el lugar de configurar la presión.
         self.l8 = QtWidgets.QLabel(self)
         self.l8.setText("SetPress:")
         self.l8.setAlignment(Qt.AlignLeft)
         self.l8.setFont(QtGui.QFont('SansSerif', 8))
         self.l8.setGeometry(170,610, 100, 20)
-
+        
+            # Se establece el lugar donde el texBox va a estar localizado
         self.tb2 = QtWidgets.QLineEdit(self)
         self.tb2.setGeometry(225,608, 50, 20)
 
-        # Creacion Boton cambio de presión
+            # Creacion Boton cambio de presión
         self.b4 = QtWidgets.QPushButton(self)
         self.b4.setText("Ok.")
         self.b4.clicked.connect(self.cambioPress)
         self.b4.setGeometry(200,630, 60, 20)
 
-        # Humedad
+        # Humedad Se crea la ventana en donde se ubica el indicativo para la humedad
         self.l9 = QtWidgets.QLabel(self)
         self.l9.setText("Humedad:")
         self.l9.setAlignment(Qt.AlignLeft)
@@ -162,25 +163,25 @@ class window(QMainWindow):
         self.BCD2.display(self.AutoclaveBog.humidity*100)
         self.BCD2.setDigitCount(5)
 
-        # CreacionLabel
+        # CreacionLabel referente al porcentaje de la humedad.
         self.l10 = QtWidgets.QLabel(self)
         self.l10.setText("%")
         self.l10.setAlignment(Qt.AlignLeft)
         self.l10.setFont(QtGui.QFont('SansSerif', 16))
         self.l10.setGeometry(405, 610, 100, 100)
 
-        # CreacionLabel
+            # CreacionLabel se establece el lugar donde la humedad se va a configurar.
         self.l11 = QtWidgets.QLabel(self)
         self.l11.setText("SetHum:")
         self.l11.setAlignment(Qt.AlignLeft)
         self.l11.setFont(QtGui.QFont('SansSerif', 8))
         self.l11.setGeometry(470,610, 100, 20)
-
+            # Se ubica el recuadro para obtener la información de la humedad.
         self.tb3 = QtWidgets.QLineEdit(self)
         self.tb3.setGeometry(525,608, 50, 20)
         
-        # Cambio de humedad
-        # CreacionBoton2
+       
+            # CreacionBoton2 para cambiar el valor de la humedad
         self.b5 = QtWidgets.QPushButton(self)
         self.b5.setText("Ok.")
         self.b5.clicked.connect(self.cambioHum)
@@ -198,43 +199,45 @@ class window(QMainWindow):
         self.l13.setGeometry(205, 30, 100,100)
         self.pixmap3 = QtGui.QPixmap("Alarma.png")
 
+            # Se verificar cada 100 milisegundos el estado de las alarmas.
         self.timer = QTimer()
         self.timer.timeout.connect(self.alarmTemp)
         self.timer.timeout.connect(self.alarmPress)
         self.timer.start(100)
         
-        
+            # Se coloca un label para indicar que la alarma está presionada
         self.l14 = QtWidgets.QLabel(self)
         self.l14.setText(str(chr(161)+"Alarma Press"+str(chr(33))))
         self.l14.setAlignment(Qt.AlignLeft)
         self.l14.setFont(QtGui.QFont('SansSerif', 12))
         self.l14.setGeometry(400, 20, 150, 20)
-
+        
         self.l15 = QtWidgets.QLabel(self)
         self.l15.setAlignment(Qt.AlignCenter)
         self.l15.setGeometry(405, 30, 100, 100)
         
-        # Recolector de datos        
+        # Recolector de datos cada t_printData milisegundos.      
         self.timer2 = QTimer()
         self.timer2.timeout.connect(self.printData)
         self.timer2.start(t_printData)
 
         
-        
+        # Se verifica si la alarma de la presión se activa
     def alarmPress(self):
         if (self.AutoclaveBog.temperature >= self.Temp_maxima):
             self.l13.setPixmap(self.pixmap3)
         else:
             self.l13.clear()
         self.update()
-
+        # Se verifica si la alarma de la temperatura está activada.
     def alarmTemp(self):
         if (self.AutoclaveBog.pressure >= self.Pressure_maxima):
             self.l15.setPixmap(self.pixmap3)
         else:
             self.l15.clear()
         self.update()
-
+        
+        # Esta función se encarga de recolectar el estado del autoclave 
     def printData(self):
         
         if self.AutoclaveBog.getTemp()>self.Temp_maxima:
@@ -261,6 +264,7 @@ class window(QMainWindow):
         print(data)
         self.update()
     
+       #Realiza la escritura en .txt
     def escritura(self,data):
         try:
             archivo= open(self.guardarDatos, "a" )
@@ -268,7 +272,7 @@ class window(QMainWindow):
             archivo.close()
         except IOError:
             pass
-        
+        # Reinicia un registro
     def reiniciarRegistro(self):
         try:
             archivo= open(self.guardarDatos, "w" )
@@ -277,21 +281,23 @@ class window(QMainWindow):
         except IOError:
             pass
         
-    
+        # Para cambiar de tempertura
     def cambioHum(self):
         input = self.tb3.text()
         input2 = float(input)
         self.BCD2.display(input2*100)
         self.AutoclaveBog.sethumidity(input2)
         self.update()
-
+        
+        # Se cambia de presión.
     def cambioPress(self):
         input = self.tb2.text()
         input2 = float(input)
         self.BCD1.display(input2)
         self.AutoclaveBog.setPressure(input2)
         self.update()
-
+        
+        # Se cambia de temperatura.
     def cambioTemp(self):
         input = self.tb1.text()
         input2 = float(input)
@@ -300,12 +306,14 @@ class window(QMainWindow):
         self.pb.setValue((input2/150)*100)
         self.update()
 
+        # Se cierra el enclave
     def cerrarEnc(self):
         if self.AutoclaveBog.close == 0:
             self.l2.setPixmap(self.pixmap1)
         self.AutoclaveBog.cerrar()
         self.update()
-
+        
+        # Se hace la simulación de un enclave abierto.
     def abrirEnc(self):
         if self.AutoclaveBog.open == 0:
             self.l2.setPixmap(self.pixmap2)
@@ -317,6 +325,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     wind = window()
     wind.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec()) # Se utliza para mantenerse en un ciclo infinito, siento sensible a los eventos.
     
     
