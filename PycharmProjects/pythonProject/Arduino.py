649c295133b78e2d383e4d532fae22ed79a964c6
Mateo <mabernalbo@unal.edu.co>
2021-04-05 20:24:41 -0500

    modificacion arduino

--- PycharmProjects/pythonProject/Arduino.py
+++ PycharmProjects/pythonProject/Arduino.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     puerto = serial.Serial("COM5",9600)
     while(True):
         if(puerto.readable()):
-            print(puerto.read())
+            print(puerto.readline())
